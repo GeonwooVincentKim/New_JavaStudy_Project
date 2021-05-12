8a1d2d006ffce7e6efe0c9b9c2deee3a38e7b4d0
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-12 16:53:35 +0900

    [Feature] - Added Tire.java file

--- src/chap07/SupersonicAirplaneExample.java
+++ src/chap07/SupersonicAirplaneExample.java
@@ -2,15 +2,25 @@ package chap07;
 
 public class SupersonicAirplaneExample {
     public static void main(String[] args) {
-        SupersonicAirplane sa = new SupersonicAirplane();
-        sa.takeOff();
+        // SupersonicAirplane sa = new SupersonicAirplane();
+        // sa.takeOff();
 
-        sa.fly();
-        sa.flyMode = SupersonicAirplane.SUPERSONIC;
-        sa.fly();
+        // sa.fly();
+        // sa.flyMode = SupersonicAirplane.SUPERSONIC;
+        // sa.fly();
 
-        sa.flyMode = SupersonicAirplane.NORMAL;
-        sa.fly();
-        sa.land();
+        // sa.flyMode = SupersonicAirplane.NORMAL;
+        // sa.fly();
+        // sa.land();
+        
+        Airplane ap = new Airplane();
+        Airplane bp = new SupersonicAirplane();
+        
+        System.out.println("1. 일반 비행기");
+        ap.takeOff();
+        System.out.println("2. 초음속 비행기");
+        bp.takeOff();
+
+        // bp.flyMode = SupersonicAirplane.SUPERSONIC; --> Compile-Error
     }
 }
---
+++ src/chap07/Tire.java
@@ -0,0 +1,11 @@
+package chap07;
+
+public class Tire {
+    public int maxRotation;
+    public int accumlatedRotation;
+    public String location;
+
+    public Tire(String location, int maxRotation){
+
+    }
+}
