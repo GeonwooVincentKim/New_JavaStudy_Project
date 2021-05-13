b88c81e25e62d34b53bf73c48bbe1e28c9de333e
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-13 09:18:42 +0900

    [Feature] - Added codes that are related to Polymorphism of Java

--- src/chap07/Tire.java
+++ src/chap07/Tire.java
@@ -6,6 +6,20 @@ public class Tire {
     public String location;
 
     public Tire(String location, int maxRotation){
+        this.location = location;
+        this.maxRotation = maxRotation;
+    }
 
+    public boolean roll(){
+        ++accumlatedRotation;
+        if(accumlatedRotation < maxRotation){
+            System.out.println(location + " Tire 수명 : " + 
+            (maxRotation - accumlatedRotation) + "회");
+            
+            return true;
+        } else {
+            System.out.println("*** " + location + " Tire 펑크 ***");
+            return false;
+        }
     }
 }
