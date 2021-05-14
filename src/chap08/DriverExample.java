0fbb9cfb595bf1ef63afe005e8d8d33e5cc0554c
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-14 13:50:49 +0900

    [Feature] - Added Taxi.java

--- src/chap08/DriverExample.java
+++ src/chap08/DriverExample.java
@@ -5,6 +5,9 @@ public class DriverExample {
         Driver driver = new Driver();
 
         Bus bus = new Bus();
+        Taxi taxi = new Taxi();
+
         driver.drive(bus);
+        driver.drive(taxi);
     }
 }
