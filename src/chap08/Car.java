7bc63a1d69d8dddb3a152f3dc0b4ff6694e0e190
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-14 11:31:56 +0900

    [Feature] - Successfully generated Tire interface

---
+++ src/chap08/Car.java
@@ -0,0 +1,7 @@
+package chap08;
+
+public class Car {
+    void run(){
+        System.out.println("차가 달려유~");
+    }
+}
---
+++ src/chap08/CarExample.java
@@ -0,0 +1,10 @@
+package chap08;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        // System.out.println(myCar);
+        myCar.run();
+        
+    }
+}
