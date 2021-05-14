3f731243511982c33d2a0e5fa97447a7f494a705
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-14 13:40:57 +0900

    [Feature] - Successfully genereated Driver.java file

---
+++ src/chap08/DriverExample.java
@@ -0,0 +1,10 @@
+package chap08;
+
+public class DriverExample {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        Bus bus = new Bus();
+        driver.drive(bus);
+    }
+}
