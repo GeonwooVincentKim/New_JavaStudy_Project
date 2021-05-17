a94836997ea6fd021c963f2da46ef7b1c895069f
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-17 10:33:42 +0900

    [Feature] - Try-Catch phrase by using Java

---
+++ src/chap10/ClassCastExceptionExample.java
@@ -0,0 +1,28 @@
+package chap10;
+
+import static java.lang.System.out;
+
+public class ClassCastExceptionExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        changeDog(dog);
+
+        Cat cat = new Cat();
+        changeCat(cat);
+    }
+
+    public static void changeDog(Animal animal){
+        Dog dog = (Dog) animal;
+        out.println("멍멍");
+    }
+    
+    public static void changeCat(Animal animal){
+        Cat cat = (Cat) animal;
+        out.println("야옹");
+    }
+}
+
+
+class Animal{}
+class Dog extends Animal {}
+class Cat extends Animal {}
