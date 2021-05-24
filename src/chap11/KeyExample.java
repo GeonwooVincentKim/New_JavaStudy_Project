7600f3196b6754ca192f92406b981a4e2ed27e5b
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-24 12:14:10 +0900

    [Feature] - Put the data into the HashTable

--- src/chap11/KeyExample.java
+++ src/chap11/KeyExample.java
@@ -6,6 +6,14 @@ import static java.lang.System.out;
 public class KeyExample {
     public static void main(String[] args) {
         HashMap<Key, String> hashMap = new HashMap<Key, String>();
+        
         out.println(hashMap);
+        out.println(hashMap.size());
+        out.println(hashMap.values());
+
+        hashMap.put(new Key(1), "홍길동");
+
+        String value = hashMap.get(new Key(1));
+        out.println(value);
     }
 }
