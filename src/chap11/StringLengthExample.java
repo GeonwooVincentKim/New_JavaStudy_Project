6c856eab2c6c4b4e48762bd977446eaf51e7bdc5
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-26 11:52:47 +0900

    [Feature] - Convert floor and ceil and round value

---
+++ src/chap11/StringLengthExample.java
@@ -0,0 +1,27 @@
+package chap11;
+
+// import java.util.Scanner;
+import static java.lang.System.out;
+
+public class StringLengthExample {
+    public static void main(String[] args) {
+        double v1 = Math.floor(6.8);
+        double v2 = Math.ceil(6.3);
+        double v3 = Math.round(6.8);
+
+        out.println(v1);
+        out.println(v2);
+        out.println(v3);
+        // Scanner sc = new Scanner(System.in);
+        // String getInput = sc.nextLine();
+
+        // int length = 0;
+        // char convertToChar[] = getInput.toCharArray();
+
+        // for(int i = 1; convertToChar[i] != '\0'; i++){
+        //     length++;
+        // }
+
+        // System.out.println(length);
+    }
+}
