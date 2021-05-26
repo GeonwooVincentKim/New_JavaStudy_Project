2dc8a68ba846f9a15dd45507867a52df03312f04
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-26 11:03:08 +0900

    [Feature] - Committing files

--- src/chap11/ReverseString.java
+++ src/chap11/ReverseString.java
@@ -3,13 +3,29 @@ package chap11;
 import static java.lang.System.out;
 
 public class ReverseString {
+    public static char[] reverseStr(String str){
+        int n = str.length();
+        char temp;
+        char reverseString[] = str.toCharArray();
+        
+        for (int i = 0; i < n / 2; i++){
+            temp = reverseString[i];
+            reverseString[i] = reverseString[reverseString.length - i - 1];
+            reverseString[reverseString.length - i - 1] = temp;
+        }
+
+        return reverseString;
+    }
+
     public static void main(String[] args) {
-        String readLine = "detartrated";
+        String readLine = "Hola Miguel!";
         out.print(readLine + " --> 뒤집기 전\n");
         
-        out.println("-- 뒤집은 후--");
-        for(int i = readLine.length(); i > 0; i--){
-            out.print(readLine.charAt(i - 1));
-        }
+        out.print(reverseStr(readLine));
+
+        // out.println("-- 뒤집은 후--");
+        // for(int i = readLine.length(); i > 0; i--){
+        //     out.print(readLine.charAt(i - 1));
+        // }
     }
 }
