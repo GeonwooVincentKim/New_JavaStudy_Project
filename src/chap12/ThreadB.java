94c367287ebef7d559115d031ffb3bc14c7ab1aa
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-27 16:06:05 +0900

    [Modify] - Not needed data

--- src/chap12/ThreadB.java
+++ src/chap12/ThreadB.java
@@ -3,7 +3,6 @@ package chap12;
 import static java.lang.System.out;
 
 public class ThreadB extends Thread {
-    @Override
     public void run(){
         for(int i = 0; i < 2; i++){
             out.println(getName() + " 가 출력한 내용");
