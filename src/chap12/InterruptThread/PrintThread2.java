22fb66d9eda48036b5646aecc3041eaad7ca0237
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-28 17:55:48 +0900

    [Update] - Updated codes in PrintThread2.java file

--- src/chap12/InterruptThread/PrintThread2.java
+++ src/chap12/InterruptThread/PrintThread2.java
@@ -4,13 +4,12 @@ import static java.lang.System.out;
 
 public class PrintThread2 extends Thread {
     public void run(){
-        try {
-            while(true){
-                out.println();
-                Thread.sleep(1);
+        while(true){
+            out.println("실행 중..");
+        
+            if(Thread.interrupted()){
+                break;
             }
-        } catch (InterruptedException e){
-            e.printStackTrace();
         }
 
         out.println("자원 정리");
