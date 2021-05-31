2dfbeac2813abc5d6773acaece6380daa610e3c7
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-31 15:08:29 +0900

    [Feature] - Trying to convert as Map

--- src/chap13/HashMapExample.java
+++ src/chap13/HashMapExample.java
@@ -4,6 +4,7 @@ import static java.lang.System.out;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapExample {
     public static void main(String[] args) {
@@ -17,5 +18,13 @@ public class HashMapExample {
         map.put("동장군", 80);
         map.put("홍길동", 95);
         out.println("총 Entry 수 : " + map.size());
+
+        out.println("\t홍길동 : " + map.get("홍길동"));
+        out.println();
+
+        Set<String> keySet = map.keySet();
+        out.println(keySet);
+
+        
     }
 }
