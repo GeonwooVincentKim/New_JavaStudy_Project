7aee5f8c3b429791310c0b4093806567a305b719
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-31 14:16:50 +0900

    [Feature] - Generated HashCode codes

---
+++ src/chap13/HashSetExample2.java
@@ -0,0 +1,21 @@
+package chap13;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.out;
+
+public class HashSetExample2 {
+    public static void main(String[] args) {
+        Set<Member> set = new HashSet<Member>();
+        out.println(set);
+
+        set.add(new Member("홍길동", 30));
+        out.println(set);
+
+        set.add(new Member("홍길동", 30));
+        out.println(set);
+
+        out.println("총 객체 수 : " + set.size());
+    }
+}
