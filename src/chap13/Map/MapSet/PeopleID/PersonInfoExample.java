2524ae144a0e1855a2bd72520867be3efe4fafb7
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-01 14:13:34 +0900

    [Feature] - Trying to find how to solve that there are no readable data in the list

--- src/chap13/Map/MapSet/PeopleID/PersonInfoExample.java
+++ src/chap13/Map/MapSet/PeopleID/PersonInfoExample.java
@@ -3,6 +3,7 @@ package chap13.Map.MapSet.PeopleID;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import static java.lang.System.out;
@@ -16,13 +17,39 @@ public class PersonInfoExample {
         map.put(new PersonDTO("myID", "myPWD", "myName", 18), "넉네임");
         out.println(map.size());
 
+        out.println("Just Just Iterator Method --> ");
+
+        int i = 1;
+
         Set<PersonDTO> keySet = map.keySet();
         Iterator<PersonDTO> keyIterator = keySet.iterator();
         while (keyIterator.hasNext()) {
             PersonDTO key = keyIterator.next();
             String value = map.get(key);
-            out.println("\t" + key.getUserID() + " " + key.getUserPWD() + " " + key.getName() + " " + key.getAge() + " "
-                    + value);
+            out.println("#" + i + " " + key.getUserID() + " " + key.getUserPWD() + " " + key.getName() + " "
+                    + key.getAge() + " " + value);
+            i++;
+        }
+
+        out.println();
+
+        out.println("Use Map Entry together");
+        Set<Entry<PersonDTO, String>> entrySet = map.entrySet();
+        out.println(entrySet);
+
+        Iterator<Entry<PersonDTO, String>> entryIter = entrySet.iterator();
+        out.println(entryIter);
+
+        for (int j = 0; j < entrySet.size(); j++) {
+            if (!keyIterator.hasNext()) {
+                out.println("Cannot get the data more because data does not exists");
+                break;
+            } else {
+                PersonDTO key = keyIterator.next();
+                String value = map.get(key);
+                out.println("#" + (j + 1) + " " + key.getUserID() + " " + key.getUserPWD() + " " + key.getName() + " "
+                        + key.getAge() + " " + value);
+            }
         }
     }
 }
