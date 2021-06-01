f3ae712134c189a4c0ec116eb519b91919049b3f
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-01 15:47:40 +0900

    [Modify] - Change data-structure

--- src/chap13/Map/MapSet/PeopleID/PersonGetData.java
+++ src/chap13/Map/MapSet/PeopleID/PersonGetData.java
@@ -8,38 +8,79 @@ import java.util.Map.Entry;
 import static java.lang.System.out;
 
 public class PersonGetData {
-    public void printUserInfoAsMap(Map<PersonDTO, String> map) {
+    // public void printUserInfoAsMap(Map<PersonDTO, String> map) {
+    // int i = 1;
+
+    // Set<PersonDTO> keySet = map.keySet();
+    // Iterator<PersonDTO> keyIterator = keySet.iterator();
+    // while (keyIterator.hasNext()) {
+    // PersonDTO key = keyIterator.next();
+    // String value = map.get(key);
+    // out.println("#" + i + " " + key.getUserID() + " " + key.getUserPWD() + " " +
+    // key.getName() + " "
+    // + key.getAge() + " " + value);
+    // i++;
+    // }
+    // }
+
+    public void printUserInfoAsMap(Map<String, PersonDTO> map) {
         int i = 1;
 
-        Set<PersonDTO> keySet = map.keySet();
-        Iterator<PersonDTO> keyIterator = keySet.iterator();
+        Set<String> keySet = map.keySet();
+        Iterator<String> keyIterator = keySet.iterator();
         while (keyIterator.hasNext()) {
-            PersonDTO key = keyIterator.next();
-            String value = map.get(key);
-            out.println("#" + i + " " + key.getUserID() + " " + key.getUserPWD() + " " + key.getName() + " "
-                    + key.getAge() + " " + value);
+            String key = keyIterator.next();
+            PersonDTO value = map.get(key);
+            out.println("#" + i + " " + key + " " + value.getUserID() + " " + value.getUserPWD() + " " + value.getName()
+                    + " " + value.getAge());
             i++;
         }
     }
 
-    public Set<Entry<PersonDTO, String>> printUserInfoAsEntry(Map<PersonDTO, String> map) {
+    // public Set<Entry<PersonDTO, String>> printUserInfoAsEntry(Map<PersonDTO,
+    // String> map) {
+    // int i = 1;
+
+    // Set<Entry<PersonDTO, String>> entrySet = map.entrySet();
+    // // out.println(entrySet);
+
+    // Iterator<Entry<PersonDTO, String>> entryIter = entrySet.iterator();
+    // // out.println(entryIter);
+
+    // while (entryIter.hasNext()) {
+    // Entry<PersonDTO, String> entry = entryIter.next();
+    // PersonDTO key = entry.getKey();
+    // String value = entry.getValue();
+    // // Cannot use this because those are just for `Map` not for `Entry<Key,
+    // Value>`
+    // // PersonDTO key = keyIterator.next();
+    // // String value = map.get(entry);
+    // out.println("#" + i + " " + key.getUserID() + " " + key.getUserPWD() + " " +
+    // key.getName() + " "
+    // + key.getAge() + " " + value);
+    // i++;
+    // }
+
+    // return entrySet;
+    // }
+    public Set<Entry<String, PersonDTO>> printUserInfoAsEntry(Map<String, PersonDTO> map) {
         int i = 1;
 
-        Set<Entry<PersonDTO, String>> entrySet = map.entrySet();
+        Set<Entry<String, PersonDTO>> entrySet = map.entrySet();
         // out.println(entrySet);
 
-        Iterator<Entry<PersonDTO, String>> entryIter = entrySet.iterator();
+        Iterator<Entry<String, PersonDTO>> entryIter = entrySet.iterator();
         // out.println(entryIter);
 
         while (entryIter.hasNext()) {
-            Entry<PersonDTO, String> entry = entryIter.next();
-            PersonDTO key = entry.getKey();
-            String value = entry.getValue();
+            Entry<String, PersonDTO> entry = entryIter.next();
+            String key = entry.getKey();
+            PersonDTO value = entry.getValue();
             // Cannot use this because those are just for `Map` not for `Entry<Key, Value>`
             // PersonDTO key = keyIterator.next();
             // String value = map.get(entry);
-            out.println("#" + i + " " + key.getUserID() + " " + key.getUserPWD() + " " + key.getName() + " "
-                    + key.getAge() + " " + value);
+            out.println("#" + i + " " + key + " " + value.getUserID() + " " + value.getUserPWD() + " " + value.getName()
+                    + " " + value.getAge());
             i++;
         }
 
--- src/chap13/Map/MapSet/PeopleID/PersonInfoExample.java
+++ src/chap13/Map/MapSet/PeopleID/PersonInfoExample.java
@@ -2,6 +2,7 @@ package chap13.Map.MapSet.PeopleID;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -10,17 +11,15 @@ import static java.lang.System.out;
 public class PersonInfoExample {
 
     public static void main(String[] args) {
-        Map<PersonDTO, String> map = new HashMap<PersonDTO, String>();
+        Map<String, PersonDTO> map = new HashMap<String, PersonDTO>();
         out.println(map);
-
-        map.put(new PersonDTO("yourID", "yourPWD", "yourName", 16), "닉네임");
-        map.put(new PersonDTO("myID", "myPWD", "myName", 18), "넉네임");
+        map.put("LaCampanella", new PersonDTO("yourID", "yourPWD", "yourName", 16));
 
         // Show when the <Key, Value> duplicates
-        map.put(new PersonDTO("myID", "myPWD", "myName", 18), "넉네임");
+        map.put("Darkness", new PersonDTO("myID", "myPWD", "myName", 18));
 
         // Convert Value, "넉네임" as "눅네임"
-        map.put(new PersonDTO("myID", "myPWD", "myName", 18), "눅네임");
+        map.put("GraphBallstics", new PersonDTO("myID", "myPWD", "myName", 18));
 
         out.println("Just Just Iterator Method --> ");
 
@@ -31,7 +30,40 @@ public class PersonInfoExample {
 
         out.println("Use Map Entry together");
 
-        Set<Entry<PersonDTO, String>> entrySet = pGD.printUserInfoAsEntry(map);
+        Set<Entry<String, PersonDTO>> entrySet = pGD.printUserInfoAsEntry(map);
         out.println("No.2 Size : " + entrySet.size());
+
+        // Compare that the ID and userPWD is already exist in the Map area or not exist
+        Scanner sc = new Scanner(System.in);
+        // String id = "LaCampanella";
+        // out.print(map.containsKey(id));
+
+        // out.print(map.containsKey(id));
+        while (true) {
+            out.println("아이디 및 비밀번호를 입력하세요.");
+
+            out.println("아이디 : ");
+            String id = sc.nextLine();
+
+            out.println("비밀번호 : ");
+            String pwd = sc.nextLine();
+
+            out.print(map);
+            // out.print(map.containsKey(id));
+
+            if (map.containsKey(id)) {
+                out.print(map.containsKey(id));
+                if (map.get(id).equals(pwd)) {
+                    out.println("로그인 되었습니다");
+                    break;
+                } else {
+                    out.println("비밀번호가 일치하지 않습니다");
+                }
+            } else {
+                out.println("입력하신 아이디가 존재하지 않습니다");
+            }
+        }
+
+        sc.close();
     }
 }
