1d28ac2a2465864b4b1001b31a88dcb3d73156f7
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-01 10:53:35 +0900

    [Feature] - Applied temp

--- src/chap13/Test.java
+++ src/chap13/Test.java
@@ -1,17 +1,41 @@
 package chap13;
 
 public class Test {
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public static void temp(int a, int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+
     public static void main(String[] args) {
-        int a = 72, b = 30;
+        // int a = 72, b = 30;
+        int a = 30, b = 72;
         System.out.println(a + " " + b);
 
-        int r = a % b;
-        System.out.println(r);
+        if (a < b) {
+            temp(a, b);
+            int t_c = gcd(a, b);
+            System.out.println(t_c);
+        } else {
+            int c = gcd(a, b);
+            System.out.println(c);
+        }
 
-        int r1 = b % r;
-        System.out.println(r1);
+        // int r = a % b;
+        // System.out.println(r);
 
-        int r2 = r % r1;
-        System.out.println(r2);
+        // int r1 = b % r;
+        // System.out.println(r1);
+
+        // int r2 = r % r1;
+        // System.out.println(r2);
     }
 }
