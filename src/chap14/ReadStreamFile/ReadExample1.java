57edc278026549685c9555a5904e284e442b8d1e
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-02 14:41:15 +0900

    [Feature] - Reads file

--- src/chap14/ReadStreamFile/ReadExample1.java
+++ src/chap14/ReadStreamFile/ReadExample1.java
@@ -8,9 +8,24 @@ import java.io.InputStream;
 public class ReadExample1 {
     public static void main(String[] args) throws Exception {
         String fLocate = "E:/NewJavaBasicProject/New_JavaProject/";
-        InputStream is = new FileInputStream(fLocate + "src/chap14/");
 
+        InputStream is = new FileInputStream(fLocate + "src/chap14/WriteStreamFile/test1.db");
         out.println(is);
+
+        byte[] buffer = new byte[100];
+        out.println(buffer);
+
+        while (true) {
+            int readByteNum = is.read(buffer);
+            out.println(readByteNum);
+
+            if (readByteNum == -1)
+                break;
+            
+            for(int i = 0; i < readByteNum; i++){
+                out.println(buffer[i]);
+            }
+        }
 
         is.close();
     }
