7e0e75fe064dcb0fbd705ed459ee7e3b724451f3
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-02 11:22:26 +0900

    [Modify] - Delete unnecessary files

--- src/chap14/WriteExample.java
+++ src/chap14/WriteExample.java
@@ -2,7 +2,6 @@ package chap14;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 
 import static java.lang.System.out;
@@ -21,23 +20,23 @@ public class WriteExample {
         return os;
     }
 
-    public static int getData(OutputStream os) {
+    public static int[] getData(OutputStream os) {
         int result[] = new int[3];
-        int getResult = 0;
 
         byte a = 10;
-        byte b = 20;
-        byte c = 30;
 
-        result[0] = a;
-        result[1] = b;
-        result[2] = c;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = a;
+            a += 10;
+            out.println(result[i]);
+        }
 
         try {
-            os.write(result[0]);
-            os.write(result[1]);
-            os.write(result[2]);
-            os.flush();
+            for (int i = 0; i < result.length; i++) {
+                os.write(result[i]);
+                out.println(result[i]);
+                os.flush();
+            }
             os.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -45,18 +44,8 @@ public class WriteExample {
             e.printStackTrace();
         }
 
-        return getResult;
+        return result;
     }
-
-    // public static void writeFile(OutputStream os, int getResult) {
-    // try {
-    // os.write(getResult);
-    // os.flush();
-    // os.close();
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
 
     public static void main(String[] args) throws Exception {
         String fLocation = "E:/NewJavaBasicProject/New_JavaProject/test1.db";
