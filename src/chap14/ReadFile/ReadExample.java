6e15476a1cb261319608f6cd1cb913328349eb69
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-02 14:23:29 +0900

    [Feature] - Testing

--- src/chap14/ReadFile/ReadExample.java
+++ src/chap14/ReadFile/ReadExample.java
@@ -3,7 +3,9 @@ package chap14.ReadFile;
 import static java.lang.System.out;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ReadExample {
     public static InputStream readFile(String fLocate) {
@@ -18,20 +20,38 @@ public class ReadExample {
         return is;
     }
 
-    public static String getData(InputStream readFile) {
-        String result = "";
+    public static void writeData(int[] getResult, String fLocate) {
+        OutputStream os = null;
+
+        try {
+            os = new FileOutputStream("E:/NewJavaBasicProject/New_JavaProject/src/chap14/" + fLocate);
+            for (int i = 0; i < getResult.length; i++) {
+                os.write(getResult[i]);
+                out.println(getResult);
+                os.flush();
+            }
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int[] getData(InputStream readFile) throws Exception {
+        // int result = readFile.read();
+        // return readLine;
+        int result[] = new int[3];
         int data = 0;
+        int i = 0;
 
         while (true) {
-            try {
-                data = readFile.read();
-                if (data == -1)
-                    break;
+            data = readFile.read();
 
-                out.println(data);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            if (data == -1)
+                break;
+
+            out.println(data);
+            result[i] = data;
+            i++;
         }
 
         return result;
@@ -40,7 +60,9 @@ public class ReadExample {
     public static void main(String[] args) throws Exception {
         String fLocate = "WriteStreamFile/test1.db";
         InputStream is = readFile(fLocate);
-        getData(is);
+        int[] getResult = getData(is);
+        // getData(is);
+        writeData(getResult, fLocate);
 
         is.close();
     }
