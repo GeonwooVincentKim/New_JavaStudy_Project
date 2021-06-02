213f647d5b513c79bea3ff239d0daffe1ab3c7b3
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-02 13:26:03 +0900

    [Feature] - Byte Output by iterating the each bytes location

---
+++ src/chap14/WriteExample2.java
@@ -0,0 +1,23 @@
+package chap14;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import static java.lang.System.out;
+
+public class WriteExample2 {
+    public static void main(String[] args) throws Exception {
+        String fLocate = "E:/NewJavaBasicProject/New_JavaProject/";
+        OutputStream os = new FileOutputStream(fLocate + "src/chap14/test3.db");
+        out.println(os);
+
+        byte[] array = { 10, 20, 30, 40, 50 };
+        out.println(array);
+        out.println(array.length);
+
+        os.write(array, 1, 3);
+
+        os.flush();
+        os.close();
+    }
+}
