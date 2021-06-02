579a30c10f6a1f265a602c79d73c9ba60d04bae1
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-02 13:16:43 +0900

    [Feature] - Modifiy WriteExample1.java file system

--- src/chap14/WriteExample1.java
+++ src/chap14/WriteExample1.java
@@ -2,21 +2,58 @@ package chap14;
 
 import static java.lang.System.out;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 public class WriteExample1 {
-    public static void main(String[] args) throws Exception {
-        String fLocation = "E:/NewJavaBasicProject/New_JavaProject/src/chap14/test2.db";
-
+    public static OutputStream createFile(String fLocation) {
         OutputStream os = null;
-        os = new FileOutputStream(fLocation);
-        out.println(os);
+        try {
+            os = new FileOutputStream(fLocation);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        byte[] array = { 10, 20, 30 };
-        os.write(array);
+        return os;
+    }
 
-        os.flush();
-        os.close();
+    public static int[] getData(OutputStream os) {
+        int result[] = new int[3];
+
+        // byte a = 10;
+        byte[] arr = { 10, 20, 30 };
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i];
+            out.println(result[i]);
+        }
+
+        try {
+            for (int i = 0; i < result.length; i++) {
+                os.write(result[i]);
+                out.println(result[i]);
+                os.flush();
+            }
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String fLocation = "E:/NewJavaBasicProject/New_JavaProject/";
+        OutputStream os = createFile(fLocation + "test0.db");
+        getData(os);
+
+        OutputStream os1 = createFile(fLocation + "/src/chap14/test1.db");
+        out.println(fLocation + "src/chap14/test1.db");
+        getData(os1);
     }
 }
