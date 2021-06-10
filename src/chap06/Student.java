5d08c146593851cd0d961a00ed3abe08a87ba5af
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-10 16:15:05 +0900

    [Modify] - Updated print result of execution process

--- src/chap06/Student.java
+++ src/chap06/Student.java
@@ -1,11 +1,15 @@
 package chap06;
 
+import static java.lang.System.out;
+
 public class Student {
     public static void main(String[] args) {
         Student s1 = new Student();
-        System.out.println("s1 변수가 Student 객체를 참조합니다.");
+        out.println(s1);
+        out.println("s1 변수가 Student 객체를 참조합니다.");
 
         Student s2 = new Student();
-        System.out.println("s2 변수 또한 Student 객체를 참조합니다.");
+        out.println(s2);
+        out.println("s2 변수 또한 Student 객체를 참조합니다.");
     }
 }
