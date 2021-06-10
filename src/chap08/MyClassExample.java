cd996db94a940c91b6a42857f91b4fb3121793b3
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-10 16:18:55 +0900

    [Modify] - Modified some codes

--- src/chap08/MyClassExample.java
+++ src/chap08/MyClassExample.java
@@ -1,25 +1,28 @@
 package chap08;
 
+import static java.lang.System.out;
+
 public class MyClassExample {
     public static void main(String[] args) {
-        System.out.println("1) ----------");
+        out.println("1) ----------");
 
         MyClass myClass1 = new MyClass();
         myClass1.rc.turnOn();
         myClass1.rc.setVolume(5);
-        
-        System.out.println("2) ----------");
+
+        out.println("2) ----------");
 
         MyClass myClass2 = new MyClass(new Audio());
+        out.println(myClass2);
         // myClass2.rc.turnOn();
         // myClass2.rc.turnOff();
-        
-        System.out.println("3) ----------");
+
+        out.println("3) ----------");
 
         MyClass myClass3 = new MyClass();
         myClass3.methodA();
 
-        System.out.println("4) ----------");
+        out.println("4) ----------");
 
         MyClass myClass4 = new MyClass();
         myClass4.methodB(new Television());
--- src/chap08/RemoteControlExample.java
+++ src/chap08/RemoteControlExample.java
@@ -1,10 +1,13 @@
 package chap08;
 
+import static java.lang.System.out;
+
 public class RemoteControlExample {
     public static void main(String[] args) {
         RemoteControl rc;
         rc = new Television();
         rc = new Audio();
+        out.println(rc);
         // System.out.println(rc);
     }
 }
