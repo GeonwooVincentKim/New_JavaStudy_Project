97129e7fefeb53663da973a2d2690369148f86a2
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-10 16:21:51 +0900

    [Modify] - Clean codes

--- src/chap10/ThrowsExample.java
+++ src/chap10/ThrowsExample.java
@@ -4,28 +4,29 @@ import static java.lang.System.out;
 
 public class ThrowsExample {
 
-    public static void findClass2(){
-        try{
-            Class clazz = Class.forName("java.lang.String2");
-        } catch(ClassNotFoundException e){
+    public static void findClass2() {
+        try {
+            // Class clazz = Class.forName("java.lang.String2");
+            Class.forName("java.lang.String2");
+        } catch (ClassNotFoundException e) {
             // e.printStackTrace();
             out.println("클래스가 없어요..");
         }
     }
 
     public static void main(String[] args) throws Exception {
-        findClass2();    
+        findClass2();
     }
 
     // public static void main(String[] args) {
-    //     try{
-    //         findClass();
-    //     } catch(ClassNotFoundException e){
-    //         out.println("클래스가 존재하지 않습니다.");
-    //     }
+    // try{
+    // findClass();
+    // } catch(ClassNotFoundException e){
+    // out.println("클래스가 존재하지 않습니다.");
+    // }
     // }
 
     // public static void findClass() throws ClassNotFoundException{
-    //     Class clazz = Class.forName("java.lang.String2");
+    // Class clazz = Class.forName("java.lang.String2");
     // }
 }
