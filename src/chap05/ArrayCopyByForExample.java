6755aba62919ef9c0e41a823440250933eddf3b0
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-10 17:18:38 +0900

    [Feature] - Copied Array

--- src/chap05/ArrayCopyByForExample.java
+++ src/chap05/ArrayCopyByForExample.java
@@ -34,23 +34,35 @@ public class ArrayCopyByForExample {
         int[] oldIntArray = new int[printLength];
         out.println(oldIntArray.length);
 
-        oldIntArray = getOldArray(oldIntArray);
-        // for (int i = 1; i <= 3; i++) {
-        // oldIntArray[i - 1] = i;
-        // out.print(oldIntArray[i - 1] + " ");
-        // }
+        // oldIntArray = getOldArray(oldIntArray);
+        extracted(oldIntArray);
 
         int[] newIntArray = new int[newPrintLength];
 
         // newIntArray = getNewArray(oldIntArray);
+        extracted2(oldIntArray, newIntArray);
+
+        out.println();
+        extracted3(newIntArray);
+    }
+
+    private static void extracted3(int[] newIntArray) {
+        for (int i = 0; i < newIntArray.length; i++) {
+            out.print(newIntArray[i] + ", ");
+        }
+    }
+
+    private static void extracted2(int[] oldIntArray, int[] newIntArray) {
         for (int i = 0; i < oldIntArray.length; i++) {
             newIntArray[i] = oldIntArray[i];
             out.print(newIntArray[i] + " ");
         }
+    }
 
-        out.println();
-        for (int i = 0; i < newIntArray.length; i++) {
-            out.print(newIntArray[i] + ", ");
+    private static void extracted(int[] oldIntArray) {
+        for (int i = 1; i <= 3; i++) {
+            oldIntArray[i - 1] = i;
+            out.print(oldIntArray[i - 1] + " ");
         }
     }
 }
