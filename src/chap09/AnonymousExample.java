dbae729f86892003e0697fbb7f6792ffad974176
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-14 10:55:55 +0900

    [Update] - Learn how to use Anonymous again

--- src/chap09/AnonymousExample.java
+++ src/chap09/AnonymousExample.java
@@ -4,14 +4,27 @@ import static java.lang.System.out;
 
 public class AnonymousExample {
     public static void main(String[] args) {
-        B1 obj = new B1();
-        A1 obj1 = obj;
+        // B1 obj = new B1();
+        // A1 obj1 = obj;
+        // out.println(obj1.getClass());
+
+        // obj.show();
+        // obj1.show();
+
         // A1 obj = new B1();
-        out.println(obj1.getClass());
-        obj.show();
-        obj1.show();
+        // obj.show();
 
         // B1 obj2 = (B1) obj;
         // obj2.show();
+
+        A1 obj0 = new A1();
+        obj0.show();
+
+        A1 obj = new A1() {
+            public void show() {
+                out.println("I'm the Best");
+            }
+        };
+        obj.show();
     }
 }
